f2bd0b76bb95abcf7d60253588d8520f074d68a8
Serhij Korochanskyj <webzepter@gmail.com>
2015-10-17 16:39:18 +0300

    Add API for viewing and creating events

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,15 +1,37 @@
-var users = require('../models/users');
+var events = require('../models/events');
 
 function getAllEvents(req, res, next) {
-    users.getOne(function(error, result) {
+    events.getAll(function (error, result) {
         if (error) {
             console.error('error running query', error);
             return next(error);
         }
-        res.json([result]);
+        res.json(result.rows);
+    });
+}
+
+function createEvent(req, res, next) {
+    var params = {
+        calendar_id: req.body.calendar_id,
+        name: req.body.name,
+        content: req.body.content,
+        range: {
+            begin: req.body.range.begin,
+            end: req.body.range.end
+        }
+    };
+
+    events.create(params, function (error, result) {
+        if (error) {
+            console.error('error running query', error);
+            return next(error);
+        }
+        res.status(201);
+        res.json(result.rows[0]);
     });
 }
 
 module.exports = {
-    index: getAllEvents
+    index: getAllEvents,
+    create: createEvent
 };
